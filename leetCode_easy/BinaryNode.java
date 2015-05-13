8fe5cdb7b8ac9956ca5e71002cbdea4060fb5478
Rashmi Brahma <rashmi.brahma@gmail.com>
2015-05-13 03:11:02 -0700

    binarynode initial commit

---
+++ leetCode_easy/BinaryNode.java
@@ -0,0 +1,12 @@
+package leetCode_easy;
+
+public class BinaryNode {
+	char val;
+	BinaryNode left;
+	BinaryNode right;
+	public BinaryNode(char c) {
+		val=c;
+		left=null;
+		right=null;
+	}
+}
--- leetCode_easy/RemoveLinkedListNode.java
+++ leetCode_easy/RemoveLinkedListNode.java
@@ -8,7 +8,7 @@ Return: 1 --> 2 --> 3 --> 4 --> 5
  */
 public class RemoveLinkedListNode {
 
-	public static ListNode removeElements(ListNode head, int val){
+	public ListNode removeElements(ListNode head, int val){
 		if(head==null){
 			return null;
 		}
@@ -26,42 +26,5 @@ public class RemoveLinkedListNode {
 		return helperHead.next;
 	}
 	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		ListNode head=new ListNode(1);
-		head.next=new ListNode(2);
-		head.next.next=new ListNode(6);
-		head.next.next.next=new ListNode(3);
-		head.next.next.next.next=new ListNode(4);
-		head.next.next.next.next.next=new ListNode(5);
-		head.next.next.next.next.next.next=new ListNode(6);
-		head.next.next.next.next.next.next.next=null;
-		ListNode current=head;
-		while(current!=null){
-			System.out.print(current.val+"-->");
-			current=current.next;
-		}
-		current=removeElements(head, 6);
-		System.out.println();
-		while(current!=null){
-			System.out.print(current.val+"-->");
-			current=current.next;
-		}
-		ListNode head1=new ListNode(1);
-		//head1.next=null;
-		/*ListNode current=head1;
-		while(current!=null){
-			System.out.print(current.val+"-->");
-			current=current.next;
-		}
-		current=removeElements(head1, 1);
-		System.out.println();
-		while(current!=null){
-			System.out.print(current.val+"-->");
-			current=current.next;
-		}*/
-	}
 
 }

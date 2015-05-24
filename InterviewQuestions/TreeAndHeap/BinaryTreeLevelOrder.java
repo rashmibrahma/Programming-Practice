65e7805eb7701569c8b431a365cf4af1687a4fa1
Rashmi Brahma <rashmi.brahma@gmail.com>
2015-05-23 20:43:14 -0700

    Update BinaryTreeLevelOrder.java

--- InterviewQuestions/TreeAndHeap/BinaryTreeLevelOrder.java
+++ InterviewQuestions/TreeAndHeap/BinaryTreeLevelOrder.java
@@ -2,6 +2,19 @@ package interviewQuestions;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/*
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as [[3], [9,20], [15,7]]
+ */
 public class BinaryTreeLevelOrder {
 	public ArrayList<ArrayList<TreeNode>> levelOrder(TreeNode root){
 		if(root==null){
